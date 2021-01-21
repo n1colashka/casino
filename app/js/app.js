7aad7fa4967c547a7d7375d3dd59842fcc0e94e8
n1colashka <deckards2505@gmail.com>
2021-01-21 16:54:49 +0300

    fix: fix

--- app/js/app.js
+++ app/js/app.js
@@ -3,35 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuClose =  document.querySelector('.header__close-btn');
     const menuNavigation =  document.querySelector('.header__navigation');
     const sublists =  document.querySelectorAll('.navigation__sublist');
+    const promocodesBtn = document.querySelectorAll('[data-promo]');
 
-    menuBtn.addEventListener('click', function() {
-        menuNavigation.classList.add('open');
-    })
-
-    menuClose.addEventListener('click', function() {
-        closeMenu();
-    })
-
-    sublists.forEach(list => {
-        list.addEventListener('click', function(e) {
-            
-            if (e.target.closest('li').classList.contains('open')) list.classList.remove('open');
-            
-            else {
-                sublists.forEach(item => {
-                    item.classList.remove('open');
-                })
-                list.classList.add('open');
-            }     
+    function initMenu() {
+        menuBtn.addEventListener('click', function() {
+            menuNavigation.classList.add('open');
         })
-        
-    })
-
-    document.body.addEventListener('click', function(e) {
-        if (!e.target.closest('.header')) {
+    
+        menuClose.addEventListener('click', function() {
             closeMenu();
-        }
-    })
+        })
+
+        sublists.forEach(list => {
+            list.addEventListener('click', function(e) {
+                
+                if (e.target.closest('li').classList.contains('open')) list.classList.remove('open');
+                
+                else {
+                    sublists.forEach(item => {
+                        item.classList.remove('open');
+                    })
+                    list.classList.add('open');
+                }     
+            })
+            
+        })
+
+        document.body.addEventListener('click', function(e) {
+            if (!e.target.closest('.header')) {
+                closeMenu();
+            }
+        })
+    }
 
     function closeMenu() {
         menuNavigation.classList.remove('open');
@@ -39,4 +42,19 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.remove('open');
         })
     }
+
+    function initPromocodeOpen() {
+        if (promocodesBtn) {
+            promocodesBtn.forEach(item => {
+                item.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    item.classList.add('open');
+                })
+            })
+        }
+    }
+
+
+    initMenu();
+    initPromocodeOpen();
 })
